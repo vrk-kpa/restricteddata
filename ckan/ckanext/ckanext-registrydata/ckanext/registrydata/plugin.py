425fd7987d7d3e59df13869d6edab26d66cccc8d
Teemu Erkkola <teemu.erkkola@gofore.com>
2023-08-14 12:08:11 +0300

    REKDAT-5: Remove debug logging

--- ckan/ckanext/ckanext-registrydata/ckanext/registrydata/plugin.py
+++ ckan/ckanext/ckanext-registrydata/ckanext/registrydata/plugin.py
@@ -2,10 +2,6 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckanext.pages.interfaces import IPagesSchema
 
-import logging
-
-
-log = logging.getLogger(__name__)
 
 class RegistrydataPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -23,12 +19,10 @@ class RegistrydataPagesPlugin(plugins.SingletonPlugin):
     plugins.implements(IPagesSchema)
 
     def update_config(self, config_):
-        log.info("REGISTRYDATA_PAGES update_config")
         toolkit.add_template_directory(config_, "templates_pages")
 
     # IPagesSchema
     def update_pages_schema(self, schema):
-        log.info("REGISTRYDATA_PAGES update_pages_schema")
         schema.update({
             'title_fi': [],
             'title_sv': [],
