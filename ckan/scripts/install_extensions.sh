61fad44aef6eff0524ca8df83bee75c38cdfa515
Teemu Erkkola <teemu.erkkola@gofore.com>
2023-08-17 12:36:12 +0300

    Optimized extension installation in docker builds

--- ckan/scripts/install_extensions.sh
+++ ckan/scripts/install_extensions.sh
@@ -3,32 +3,6 @@ set -e
 
 echo "install_extensions ..."
 
-pip_install() {
-  if [[ ! -f "$1" ]]; then
-    echo "pip_install: skipping $1 because file does not exist!"
-    return 0
-  fi
-
-  echo "pip_install: installing $1 ..."
-
-  pip install -r "$1"
-}
-
-# install extension requirements
-pip_install "${EXT_DIR}/ckanext-dcat/requirements.txt"
-pip_install "${EXT_DIR}/ckanext-fluent/requirements.txt"
-pip_install "${EXT_DIR}/ckanext-registrydata/requirements.txt"
-pip_install "${EXT_DIR}/ckanext-scheming/requirements.txt"
-pip_install "${EXT_DIR}/ckanext-pages/requirements.txt"
-pip_install "${EXT_DIR}/ckanext-markdown_editor/requirements.txt"
-
-# install extension pip requirements
-pip_install "${EXT_DIR}/ckanext-dcat/pip-requirements.txt"
-pip_install "${EXT_DIR}/ckanext-fluent/pip-requirements.txt"
-pip_install "${EXT_DIR}/ckanext-registrydata/pip-requirements.txt"
-pip_install "${EXT_DIR}/ckanext-scheming/pip-requirements.txt"
-pip_install "${EXT_DIR}/ckanext-markdown_editor/pip-requirements.txt"
-
 # install extensions
 pip install -e ${EXT_DIR}/ckanext-dcat \
     -e ${EXT_DIR}/ckanext-fluent \
