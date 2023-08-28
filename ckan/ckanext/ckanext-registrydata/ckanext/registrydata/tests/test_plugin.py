b3d365260153bac7ca75e161b883d090fbe7919c
Teemu Erkkola <teemu.erkkola@gofore.com>
2023-08-28 12:41:23 +0300

    REKDAT-3: More granular tests

--- ckan/ckanext/ckanext-registrydata/ckanext/registrydata/tests/test_plugin.py
+++ ckan/ckanext/ckanext-registrydata/ckanext/registrydata/tests/test_plugin.py
@@ -49,7 +49,7 @@ To temporary patch the CKAN configuration for the duration of a test you can use
 """
 import pytest
 # import ckanext.registrydata.plugin as plugin
-from ckan.plugins import plugin_loaded
+from ckan.plugins import plugin_loaded, toolkit
 from ckan.tests.factories import Dataset, Sysadmin
 from ckan.tests.helpers import call_action
 
@@ -60,47 +60,228 @@ def test_plugin():
     assert plugin_loaded("registrydata_pages")
 
 
-@pytest.mark.usefixtures("clean_db", "with_plugins")
-def test_insert_dataset():
-    user = Sysadmin()
-    dataset_fields = dict(
+def minimal_dataset_with_one_resource_fields(user):
+    return dict(
         user=user,
+        private=False,
         name='test-dataset',
         title_translated={'fi': 'Test', 'sv': 'Test'},
         notes_translated={'fi': 'Test', 'sv': 'Test'},
         access_rights='non-public',
-        rights_translated={'fi': 'rights', 'sv': 'rights'},
         maintainer='maintainer',
-        maintainer_email='maintainer@example.com',
-        maintainer_website='https://example.com',
-        license_id='undefined',
+        maintainer_email=['maintainer@example.com'],
+        license_id='notspecified',
         keywords={'fi': ['test'], 'sv': ['test']},
-        highvalue='true',
-        external_urls='https://example.com',
-        update_frequency={'fi': ['daily'], 'sv': ['daily']},
-        valid_from='2023-01-01',
-        valid_till='2033-01-01',
-        private=False,
         resources=[dict(
-            name_translated={'fi': 'Test', 'sv': 'Test'},
             url='http://example.com',
-            format='test',
-            size=100,
-            rights_translated={'fi': 'rights', 'sv': 'rights'},
-            endpoint_url='https://example.com',
-            private=False,
-            maturity='current',
-            description_translated={'fi': 'Test', 'sv': 'Test'},
-            position_info='WGS84',
-            temporal_granularity={'fi': ['päivä'], 'sv': ['dag']},
-            temporal_coverage_from='2023-01-01',
-            temporal_coverage_to='2023-01-01',
-            geographical_accuracy=3
         )]
     )
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_minimal_dataset():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
     Dataset(**dataset_fields)
 
-    dataset = call_action('package_show', {'user': user}, id='test-dataset')
+    datasets = call_action('package_search', {}, {'q': ''})
+    assert len(datasets) == 1
+    dataset = datasets[0]
 
-    for key, value in dataset_fields.items():
-        assert dataset[key] == value
+    assert dataset['name'] == dataset_fields['name']
+    assert dataset['title'] == dataset_fields['title_translated']['fi']
+    assert dataset['title_translated'] == dataset_fields['title_translated']
+    assert dataset['notes'] == dataset_fields['notes_translated']['fi']
+    assert dataset['notes_translated'] == dataset_fields['notes_translated']
+    assert dataset['access_rights'] == dataset_fields['access_rights']
+    assert dataset['maintainer'] == dataset_fields['maintainer']
+    assert dataset['maintainer_email'] == dataset_fields['maintainer_email']
+    assert dataset['license_id'] == dataset_fields['license_id']
+    assert dataset['keywords'] == dataset_fields['keywords']
+
+    assert len(dataset['resources']) == 1
+    resource = dataset['resources'][0]
+    assert resource['url'] == dataset_fields['resources'][0]['url']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_rights():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['rights_translated'] = {'fi': 'Test', 'sv': 'Test'}
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['rights_translated'] == dataset_fields['rights_translated']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_maintainer_website():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['maintainer_website'] = 'http://example.com'
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['maintainer_website'] == dataset_fields['maintainer_website']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_highvalue():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['highvalue'] = True
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['highvalue'] == dataset_fields['highvalue']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_external_ursl():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['external_urls'] = ['http://example.com', 'https://example.com']
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['external_urls'] == dataset_fields['external_urls']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_update_frequency():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['update_frequency'] = {'fi': 'Test', 'sv': 'Test'}
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['update_frequency'] == dataset_fields['update_frequency']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_valid_from():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['valid_from'] = '2023-01-01'
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['valid_from'] == dataset_fields['valid_from']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_valid_till():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['valid_till'] = '2033-01-01'
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['valid_till'] == dataset_fields['valid_till']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_name():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['name_translated'] = {'fi': 'Test',
+                                                         'sv': 'Test'}
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['name_translated'] == dataset_fields['name_translated']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_format():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['format'] = 'CSV'
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['format'] == dataset_fields['format']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_size():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['size'] = 31415
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['size'] == dataset_fields['size']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_rights():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['rights_translated'] = {'fi': 'Test',
+                                                           'sv': 'Test'}
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['rights_translated'] == dataset_fields['rights_translated']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_private():
+    user = Sysadmin()
+    dataset_fields = minimal_dataset_with_one_resource_fields(user)
+    dataset_fields['resources'][0]['private'] = True
+    Dataset(**dataset_fields)
+
+    try:
+        call_action('package_show', {}, {'id': dataset_fields['name']})
+        assert False  # action should throw NotAuthorized
+    except toolkit.NotAuthorized:
+        pass
+
+    dataset = call_action('package_show', {'user': user},
+                          {'id': dataset_fields['name']})
+    assert dataset['private'] == dataset_fields['private']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_maturity():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['maturity'] = 'current'
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['maturity'] == dataset_fields['maturity']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_description():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['description_translated'] = {'fi': 'Test',
+                                                                'sv': 'Test'}
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['description_translated'] == dataset_fields['description_translated']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_position_info():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['position_info'] = 'WGS84'
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['position_info'] == dataset_fields['position_info']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_temporal_granularity():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['temporal_granularity'] = {'fi': 'Test',
+                                                              'sv': 'Test'}
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['temporal_granularity'] == dataset_fields['temporal_granularity']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_temporal_coverage_from():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['temporal_coverage_from'] = '2023-01-01'
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['temporal_coverage_from'] == dataset_fields['temporal_coverage_from']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_temporal_coverage_till():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['temporal_coverage_till'] = '2033-01-01'
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['temporal_coverage_till'] == dataset_fields['temporal_coverage_till']
+
+
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+def test_dataset_with_resource_with_geographical_accuracy():
+    dataset_fields = minimal_dataset_with_one_resource_fields(Sysadmin())
+    dataset_fields['resources'][0]['geographical_accuracy'] = 5
+    Dataset(**dataset_fields)
+    dataset = call_action('package_show', {}, {'id': dataset_fields['name']})
+    assert dataset['geographical_accuracy'] == dataset_fields['geographical_accuracy']
