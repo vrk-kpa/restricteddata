b7ff6c221c24f966a599d512f3cb67b661d63c92
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2023-09-20 12:52:24 +0300

    Add new domains to domain stacks

--- cdk/lib/domain-stack.ts
+++ cdk/lib/domain-stack.ts
@@ -5,18 +5,25 @@ import {DomainStackProps} from "./domain-stack-props";
 
 export class DomainStack extends cdk.Stack {
   readonly publicZone: aws_route53.PublicHostedZone;
+  readonly newPublicZone: aws_route53.PublicHostedZone;
   constructor(scope: Construct, id: string, props: DomainStackProps) {
     super(scope, id, props);
 
     this.publicZone = new aws_route53.PublicHostedZone(this, "HostedZone", {
       zoneName: "rekisteridata.fi",
     })
+
+    this.newPublicZone = new aws_route53.PublicHostedZone(this, "NewPublicHostedZone", {
+      zoneName: "suojattudata.fi"
+    })
+
     if (props.crossAccountId) {
       const role = new aws_iam.Role(this, 'Route53CrossDelegateRole', {
         assumedBy: new aws_iam.AccountPrincipal(props.crossAccountId),
         roleName: "Route53CrossDelegateRole"
       })
 
+      this.publicZone.grantDelegation(role)
       this.publicZone.grantDelegation(role)
     }
   }
