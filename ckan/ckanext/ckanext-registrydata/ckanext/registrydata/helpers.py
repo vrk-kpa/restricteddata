64112501f498a8f8ab6650b28caf429b6a3eec3f
Eetu Mansikkamäki <eetu.mansikkamaki@gofore.com>
2023-09-22 09:56:18 +0200

    Clean up unused site_name var

--- ckan/ckanext/ckanext-registrydata/ckanext/registrydata/helpers.py
+++ ckan/ckanext/ckanext-registrydata/ckanext/registrydata/helpers.py
@@ -135,10 +135,6 @@ def get_homepage_news(count=4, cache_duration=timedelta(days=1), language=None):
     return news[:count]
 
 
-def get_site_name():
-    return toolkit.config.get('ckanext.registrydata.site_name')
-
-
 def get_homepage_groups():
     return toolkit.get_action('group_list')({}, {
         'all_fields': True,
