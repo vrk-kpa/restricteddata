72fd50525eae4009081b8aed7dce064fe62e9961
Teemu Erkkola <teemu.erkkola@gofore.com>
2024-09-19 11:57:20 +0300

    REKDAT-179: Restrict organization member management to sysadmins

--- ckan/ckanext/ckanext-restricteddata/ckanext/restricteddata/logic/auth.py
+++ ckan/ckanext/ckanext-restricteddata/ckanext/restricteddata/logic/auth.py
@@ -3,25 +3,33 @@ from ckan.types import Context, DataDict, AuthResult, AuthFunction
 
 @toolkit.chained_auth_function
 def member_create(next_auth: AuthFunction, context: Context, data_dict: DataDict) -> AuthResult:
-    if data_dict['object_type'] == 'package':
-        try:
-            toolkit.check_access('package_update', context, {'id': data_dict['object']})
-        except toolkit.NotAuthorized:
-            return {'success': False,
-                    'message': 'Only dataset owners can modify dataset groups'}
-
-    return next_auth(context, data_dict)
+    match data_dict['object_type']:
+        case 'package':
+            try:
+                toolkit.check_access('package_update', context, {'id': data_dict['object']})
+                return next_auth(context, data_dict)
+            except toolkit.NotAuthorized:
+                return {'success': False,
+                        'message': 'Only dataset owners can modify dataset groups'}
+        case 'user':
+            return sysadmin_only(context, data_dict)
+        case _:
+            return next_auth(context, data_dict)
 
 @toolkit.chained_auth_function
 def member_delete(next_auth: AuthFunction, context: Context, data_dict: DataDict) -> AuthResult:
-    if data_dict['object_type'] == 'package':
-        try:
-            toolkit.check_access('package_update', context, {'id': data_dict['object']})
-        except toolkit.NotAuthorized:
-            return {'success': False,
-                    'message': 'Only dataset owners can modify dataset groups'}
-
-    return next_auth(context, data_dict)
+    match data_dict['object_type']:
+        case 'package':
+            try:
+                toolkit.check_access('package_update', context, {'id': data_dict['object']})
+                return next_auth(context, data_dict)
+            except toolkit.NotAuthorized:
+                return {'success': False,
+                        'message': 'Only dataset owners can modify dataset groups'}
+        case 'user':
+            return sysadmin_only(context, data_dict)
+        case _:
+            return next_auth(context, data_dict)
 
 
 def sysadmin_only(contaxt, data_dict):
