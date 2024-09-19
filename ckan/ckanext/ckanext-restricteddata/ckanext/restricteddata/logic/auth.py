b7e289310ea0abd33a744eddf12efb4713d15444
Teemu Erkkola <teemu.erkkola@gofore.com>
2024-09-19 12:03:04 +0300

    REKDAT-179: Refactoring

--- ckan/ckanext/ckanext-restricteddata/ckanext/restricteddata/logic/auth.py
+++ ckan/ckanext/ckanext-restricteddata/ckanext/restricteddata/logic/auth.py
@@ -7,14 +7,13 @@ def member_create(next_auth: AuthFunction, context: Context, data_dict: DataDict
         case 'package':
             try:
                 toolkit.check_access('package_update', context, {'id': data_dict['object']})
-                return next_auth(context, data_dict)
             except toolkit.NotAuthorized:
                 return {'success': False,
                         'message': 'Only dataset owners can modify dataset groups'}
         case 'user':
             return sysadmin_only(context, data_dict)
-        case _:
-            return next_auth(context, data_dict)
+
+    return next_auth(context, data_dict)
 
 @toolkit.chained_auth_function
 def member_delete(next_auth: AuthFunction, context: Context, data_dict: DataDict) -> AuthResult:
@@ -22,14 +21,13 @@ def member_delete(next_auth: AuthFunction, context: Context, data_dict: DataDict
         case 'package':
             try:
                 toolkit.check_access('package_update', context, {'id': data_dict['object']})
-                return next_auth(context, data_dict)
             except toolkit.NotAuthorized:
                 return {'success': False,
                         'message': 'Only dataset owners can modify dataset groups'}
         case 'user':
             return sysadmin_only(context, data_dict)
-        case _:
-            return next_auth(context, data_dict)
+
+    return next_auth(context, data_dict)
 
 
 def sysadmin_only(contaxt, data_dict):
