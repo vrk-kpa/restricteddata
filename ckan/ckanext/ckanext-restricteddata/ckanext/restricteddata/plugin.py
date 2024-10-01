3a8ab1c05e22c6fe43bb1318335179f639d8ab94
Teemu Erkkola <teemu.erkkola@gofore.com>
2024-10-01 12:34:10 +0300

    REKDAT-179: Sysadmin-only organization member delete view

--- ckan/ckanext/ckanext-restricteddata/ckanext/restricteddata/plugin.py
+++ ckan/ckanext/ckanext-restricteddata/ckanext/restricteddata/plugin.py
@@ -219,6 +219,7 @@ class RestrictedDataPlugin(plugins.SingletonPlugin, DefaultTranslation):
             'member_delete': auth.member_delete,
             'member_list': auth.member_list,
             'organization_member_create': auth.sysadmin_only,
+            'organization_member_delete': auth.sysadmin_only,
             'organization_member_list': auth.sysadmin_only,
             'api_token_create': auth.sysadmin_only,
             'user_list': auth.sysadmin_only
