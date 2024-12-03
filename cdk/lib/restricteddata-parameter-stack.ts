dc774f3924ab4e4c4983898c3e7bceef2b3c1c3e
Teemu Erkkola <teemu.erkkola@gofore.com>
2024-12-03 13:25:15 +0200

    REKDAT-361: Add authSourceAddress parameter back to avoid a dependency issue in cdk

--- cdk/lib/restricteddata-parameter-stack.ts
+++ cdk/lib/restricteddata-parameter-stack.ts
@@ -4,11 +4,19 @@ import {Construct} from "constructs";
 import {EnvStackProps} from "./env-stack-props";
 
 export class RestricteddataParameterStack extends Stack {
+  // Deprecated, included temporarily to sever dependency
+  readonly authSourceAddress: aws_ssm.IStringParameter;
   readonly authSourceAddresses: aws_ssm.IStringListParameter;
 
   constructor(scope: Construct, id: string, props: EnvStackProps ) {
     super(scope, id, props);
 
+    this.authSourceAddress = new aws_ssm.StringParameter(this, 'authSourceAddress', {
+      stringValue: '127.0.0.1',
+      description: 'Authentication source IP address',
+      parameterName: `/${props.environment}/restricteddata/auth_source_address`
+    })
+    
     this.authSourceAddresses = new aws_ssm.StringListParameter(this, 'authSourceAddresses', {
       stringListValue: ['127.0.0.1'],
       description: 'Authentication source IP address list',
