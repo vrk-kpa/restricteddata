800dd6ce2e4f7006a036ea805a4efb026b3eeae0
Teemu Erkkola <teemu.erkkola@gofore.com>
2025-11-26 09:22:05 +0200

    REKDAT-262: Ensure activity database tables are initialized in tests

--- ckan/ckanext/ckanext-restricteddata/ckanext/restricteddata/tests/conftest.py
+++ ckan/ckanext/ckanext-restricteddata/ckanext/restricteddata/tests/conftest.py
@@ -5,3 +5,4 @@ import pytest
 def clean_db(reset_db, migrate_db_for):
     reset_db()
     migrate_db_for("restricteddata")
+    migrate_db_for("activity")  # https://github.com/ckan/ckan/issues/8540
